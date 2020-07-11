54276c2f70d4977c3c2cf435dc2146902973f5d3
Jo <jolli644@gmail.com>
2020-07-11 10:00:23 +0200

    WLocker

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-const ua = window.navigator.userAgent.toLowerCase();
-const isChrome = /chrome|crios/.test(ua) && !/edge|opr\//.test(ua)
-const isBrave = isChrome && !window.googletag;
-
+var isBrave = false;
+if (window.Brave && navigator.brave && navigator.brave.isBrave) {
+    isBrave = 'probable';//or some other value, as you wish
+    navigator.brave.isBrave().then(function (r) {
+        if (r) isBrave = true;
+    });
+}
 
 // Chrome 1 - 79
 var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
 // Edge (based on chromium) detection
 var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
 
@@ -19,63 +21,78 @@ setTimeout(function () {
         setTimeout(function () {
             btn.append(
                 '<a class="btn btn-success btn-xl js-scroll-trigger" href="#chromeURL">' +
-                '            <img style="width: 30px;height: 30px" src="img/edge.png">' +
-                '                Download for Brave' +
+                '            <img style="width: 30px;height: 30px" src="img/edgec.png">' +
+                '                Download for Edge Chromium' +
                 '</a>'
             )
         }, 200);
 
     } else {
-        if (dt.browser.family.toLowerCase().includes("chrome")) {
-            btn.empty();
-            setTimeout(function () {
-                btn.append(
-                    '<a class="btn btn-success btn-xl js-scroll-trigger" href="#chromeURL">' +
-                    '            <img style="width: 30px;height: 30px" src="img/chrome.svg">' +
-                    '                Download for Chrome' +
-                    '</a>'
-                )
-            }, 200);
+        setTimeout(function () {
+            if (isBrave) {
+                btn.empty();
+                setTimeout(function () {
+                    btn.append(
+                        '<a class="btn btn-success btn-xl js-scroll-trigger" href="#chromeURL">' +
+                        '            <img style="width: 30px;height: 30px" src="img/brave.png">' +
+                        '                Download for Brave' +
+                        '</a>'
+                    )
+                }, 200);
+            }
+        }, 1200);
+        setTimeout(function () {
+            if (dt.browser.family.toLowerCase().includes("chrome")) {
+                btn.empty();
+                setTimeout(function () {
+                    btn.append(
+                        '<a class="btn btn-success btn-xl js-scroll-trigger" href="#chromeURL">' +
+                        '            <img style="width: 30px;height: 30px" src="img/chrome.svg">' +
+                        '                Download for Chrome' +
+                        '</a>'
+                    )
+                }, 200);
 
-        }
+            }
 
-        if (dt.browser.family.toLowerCase().includes("opera")) {
-            btn.empty();
-            setTimeout(function () {
-                btn.append(
-                    '<a class="btn btn-success btn-xl js-scroll-trigger" href="#operaURL">' +
-                    '            <img style="width: 30px;height: 30px" src="img/opera.svg">' +
-                    '                Download for Opera' +
-                    '</a>'
-                )
-            }, 200);
+            if (dt.browser.family.toLowerCase().includes("opera")) {
+                btn.empty();
+                setTimeout(function () {
+                    btn.append(
+                        '<a class="btn btn-success btn-xl js-scroll-trigger" href="#operaURL">' +
+                        '            <img style="width: 30px;height: 30px" src="img/opera.svg">' +
+                        '                Download for Opera' +
+                        '</a>'
+                    )
+                }, 200);
 
-        }
+            }
 
-        if (dt.browser.family.toLowerCase().includes("edge")) {
-            btn.empty();
-            setTimeout(function () {
-                btn.append(
-                    '<a class="btn btn-success btn-xl js-scroll-trigger" href="#edgeURL">' +
-                    '            <img style="width: 30px;height: 30px" src="img/edge.png">' +
-                    '                Download for Edge' +
-                    '</a>'
-                )
-            }, 200);
+            if (dt.browser.family.toLowerCase().includes("edge")) {
+                btn.empty();
+                setTimeout(function () {
+                    btn.append(
+                        '<a class="btn btn-success btn-xl js-scroll-trigger" href="#edgeURL">' +
+                        '            <img style="width: 30px;height: 30px" src="img/edge.svg">' +
+                        '                Download for Edge' +
+                        '</a>'
+                    )
+                }, 200);
 
-        }
+            }
 
-        if (dt.browser.family.toLowerCase().includes("firefox")) {
-            btn.empty();
-            setTimeout(function () {
-                btn.append(
-                    '<a class="btn btn-success btn-xl js-scroll-trigger" href="#firefoxUrl">' +
-                    '            <img style="width: 30px;height: 30px" src="img/firefox.png">' +
-                    '                Download for Firefox' +
-                    '</a>'
-                )
-            }, 200);
+            if (dt.browser.family.toLowerCase().includes("firefox")) {
+                btn.empty();
+                setTimeout(function () {
+                    btn.append(
+                        '<a class="btn btn-success btn-xl js-scroll-trigger" href="#firefoxUrl">' +
+                        '            <img style="width: 30px;height: 30px" src="img/firefox.png">' +
+                        '                Download for Firefox' +
+                        '</a>'
+                    )
+                }, 200);
 
-        }
+            }
+        }, 1500)
     }
 }, 2000)
